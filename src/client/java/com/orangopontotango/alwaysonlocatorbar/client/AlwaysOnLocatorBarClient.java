5d7d3b187f96bcd055f1efa60d310525005bfd3c
Lukas <itslukasreis@gmail.com>
2026-05-01 17:26:48 +0100

    strip template down to client only

--- src/client/java/com/orangopontotango/alwaysonlocatorbar/client/AlwaysOnLocatorBarClient.java
+++ src/client/java/com/orangopontotango/alwaysonlocatorbar/client/AlwaysOnLocatorBarClient.java
@@ -1,10 +1,15 @@
 package com.orangopontotango.alwaysonlocatorbar.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AlwaysOnLocatorBarClient implements ClientModInitializer {
+	public static final String MOD_ID = "always-on-locator-bar";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		LOGGER.info("Always On Locator Bar initialized");
 	}
 }
